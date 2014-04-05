69966b78e25c373e00d87308259d1a693e12b88c
cwsoft <info@cwsoft.de>
2014-04-06 00:02:49 +0200

    Updated fix_shebang_lines.py
    - added support for Python/Scripts/*.exe files created by PIP

--- WSPPDE/Repository/fix_shebang_lines.py
+++ WSPPDE/Repository/fix_shebang_lines.py
@@ -1,49 +1,76 @@
-#! python.exe
+#!python.exe
 '''
 #######################################################################################
 # Module: fix_shebang_lines
-# Removes the path information to the Python interpreter from all shebang lines to
-# avoid possible issues when shifting WSPPDE to another location.
+# Removes the path information from the shebang lines of all *.py and *.exe files 
+# located in the specified folders to allow moving WSPPDE to other locations as well.
 #
 # @package:   WSPPDE
 # @author:    cwsoft (http://cwsoft.de)
 # @copyright: cwsoft
 # @license:   BSD License (http://www.opensource.org/licenses/bsd-license.php)
 # @python:    2.7.x
-#
-# NOTE: Used TABS for block indentation, so do not mess up with SPACES
 #######################################################################################
 '''
-import fileinput
 import glob
 import os
+import re
 import sys
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
-def update_shebang_lines(path, shebang_line="#! python.exe\n"):
-    '''Updates shebang lines of all Python files located in path with given string'''
-    python_files = glob.glob(path + "\*.py")
-    if not python_files:
-        print "No python scripts found in %r" % os.path.abspath(path)
-        return
 
-    for python_file in python_files:    
-        print "> processing: %r" % python_file
+def fix_shebang_lines(search_path, new_shebang_line="#!python.exe", verbose=True):
+    '''
+    Removes path information from the shebang lines of all *.py and *.exe files
+    located in the given search path (non recursive).
+    '''
+    # regulare expression to detect shebang line with path information
+    reobj = re.compile(r"#!(.+)python\.exe")
+    
+    # list of all files matching *.py or *.exe in given path
+    filenames = glob.glob(search_path + r"\*.py")
+    filenames = filenames + glob.glob(search_path + r"\*.exe")
+    
+    for filename in filenames:
+        # set read/write mode depending on file extension
+        mode_read = "rb" if filename.endswith(".exe") else "r"
+        mode_write = mode_read.replace("r", "w")
 
-        file_handle = fileinput.input(files=python_file, inplace=1)
-        for line in file_handle:
-            if line.startswith("#!") and "python" in line:
-                line = shebang_line
-            print line,
+        with open(filename, mode_read) as input_file:
+            # read file content in memory
+            content = input_file.read()
+            input_file.close()
+            
+            if verbose: 
+                print "> scanning: %s" % filename,
+                
+            # check if a shebang line with path information exists
+            match = reobj.search(content)
+            if match and match.group(1):
+                # replace shebang line with new one
+                old_shebang_line = match.group()
+                content = content.replace(old_shebang_line, new_shebang_line)
+                
+                # write modified content back to file
+                with open(filename, mode_write) as output_file:
+                    output_file.write(content)
+                    output_file.close()
+                    
+                    if verbose:
+                        print " ... fixed"
+            else: 
+                if verbose:
+                    print " ... ok"
+            
 
 if __name__ == "__main__":
-    # search pathes to be scanned
+    # define search pathes containing files to fix
     search_pathes = (
-        os.path.join(os.path.dirname(sys.executable), "Scripts"),
-        os.path.join(os.path.dirname(sys.executable), "Lib\site-packages"),
+        os.path.join(os.path.dirname(sys.executable), r"Scripts"),
+        #os.path.join(os.path.dirname(sys.executable), r"Lib\site-packages")
     )
     
-    # fix shebang lines in all Python files located in search pathes    
+    # fix shebang lines in all *.py, *.exe files found in given path (non recursive)
     for search_path in search_pathes:
-        update_shebang_lines(search_path)
+        fix_shebang_lines(search_path)
